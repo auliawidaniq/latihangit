dfbf16e3c8e691669e0bd3622e82bada200180d3
auliawidaniq <58240200+auliawidaniq@users.noreply.github.com>
2021-01-21 18:11:47 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
-	public static void main(Strings[] args){
+	public static void main(String[] args){
 		System.out.println("Judul");
 		System.out.println("auliawidaniq: Aplikasi Java 2020");
 		System.out.println("--- *** ---");
